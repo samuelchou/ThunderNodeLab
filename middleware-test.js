4c0d4d4336214219dc7cd402ab2f69981fe51261
Samuel Chou <samuelchou@hotmail.com.tw>
2022-05-27 16:38:09 +0800

    feat(middleware): 示範更多 Middleware 內容。

--- middleware-test.js
+++ middleware-test.js
@@ -2,19 +2,28 @@ const express = require('express')
 const app = express()
 
 const myLogger = function (req, res, next) {
-    console.log('LOGGED')
+    console.log('Server middleware')
     next()
 }
 
 app.use(myLogger)
 
 app.get('/', (req, res) => {
+    console.log('Server say hi')
     res.send('Hello World!')
 })
 
 app.get('/users/:userId', (req, res) => {
     res.send(`you are now querying ${req.params.userId}`)
 })
+
+
+const hiddenMiddleware = function (req, res, next) {
+    console.log('This will never trigger')
+    next()
+}
+
+app.use(hiddenMiddleware)
 
 app.listen(3000)
 
